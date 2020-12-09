ef5f6dc0c33ae1dcf5b632e8f09e2db117962cd8
Dmitry Grishchuk <dmitrygrishuk@gmail.com>
2020-12-09 19:08:07 +0800

    create: validate form and modal

--- js/main.js
+++ js/main.js
@@ -54,4 +54,25 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+
+  // Обработка форм
+  $('.form').each(function() {
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "At least two letters"
+      },
+      email: {
+        required: "We need your email address to contact you",
+        email: "Your email address must be in the format of name@domain.com"
+      },
+      phone: {
+        required: "Please specify your phone",
+      },
+    },
+  });
+  })
+
 });
